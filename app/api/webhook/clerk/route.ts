8697b750decc5a7d3d8bf3ba4de0d34c68b95a0b
YoruLi <lionel3327@gmail.com>
2024-01-04 15:09:44 -0300

    ...

--- app/api/webhook/clerk/route.ts
+++ app/api/webhook/clerk/route.ts
@@ -62,6 +62,7 @@ export async function POST(req: Request) {
     };
 
     const newUser = await createUser(user);
+    console.log(newUser);
 
     if (newUser) {
       await clerkClient.users.updateUserMetadata(id, {
