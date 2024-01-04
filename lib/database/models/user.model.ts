63a174cfde63769520b2cdc02feed95497999526
YoruLi <lionel3327@gmail.com>
2024-01-04 15:08:56 -0300

    ...

--- lib/database/models/user.model.ts
+++ lib/database/models/user.model.ts
@@ -1,28 +1,12 @@
 import { Schema, SchemaDefinition, model, models } from "mongoose";
 
 const UserSchema = new Schema<SchemaDefinition>({
-  clerkId: {
-    type: String,
-    required: true,
-  },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-  },
+  clerkId: { type: String, required: true, unique: true },
+  email: { type: String, required: true, unique: true },
   username: { type: String, required: true, unique: true },
-  firstname: {
-    type: String,
-    required: true,
-  },
-  lastname: {
-    type: String,
-    required: true,
-  },
-  photo: {
-    type: String,
-    required: true,
-  },
+  firstName: { type: String, required: true },
+  lastName: { type: String, required: true },
+  photo: { type: String, required: true },
 });
 
 const User = models.User || model("User", UserSchema);
