4a9c8ba898f6ccd27d2d1cfd32f92358b5540dc1
YoruLi <lionel3327@gmail.com>
2024-01-08 23:59:07 -0300

    feat: fix svg type

--- data/svgs.ts
+++ data/svgs.ts
@@ -1,4 +1,11 @@
-const svgs = {
+interface Svg {
+  [key: string]: {
+    path: string;
+    viewBox: string;
+  };
+}
+
+const svgs: Svg = {
   home: {
     path: "M12.74 2.32a1 1 0 0 0-1.48 0l-9 10A1 1 0 0 0 3 14h2v7a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1v-7h2a1 1 0 0 0 1-1 1 1 0 0 0-.26-.68z",
     viewBox: "0 0 24 24",
